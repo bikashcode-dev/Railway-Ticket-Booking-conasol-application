3b4983a2fb14356c49e32fe7e19e612549db8430
Bikash sah <15850882+bikashsah1@users.noreply.github.com>
2026-05-16 23:57:26 +0530

    Merge remote-tracking branch 'origin/main'

--- src/entites/Train.java
+++ src/entites/Train.java
@@ -2,5 +2,4 @@ package entites;
 
 public class Train {
 
-
 }
--- src/entites/User.java
+++ src/entites/User.java
@@ -8,4 +8,44 @@ public class User {
     private String hashPassword;
     private List<Ticket> ticketsBooking;
     private String userId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public List<Ticket> getTicketsBooking() {
+        return ticketsBooking;
+    }
+
+    public void setTicketsBooking(List<Ticket> ticketsBooking) {
+        this.ticketsBooking = ticketsBooking;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
