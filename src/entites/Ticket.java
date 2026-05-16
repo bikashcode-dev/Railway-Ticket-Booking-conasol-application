ef444e72f73bec7670c5c6d33e168877373a5147
Bikash sah <15850882+bikashsah1@users.noreply.github.com>
2026-05-16 23:52:40 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/entites/Ticket.java
@@ -0,0 +1,9 @@
+package entites;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String ticketDate;
+    private String dateOfTravel;
+    private Train train ;
+}
