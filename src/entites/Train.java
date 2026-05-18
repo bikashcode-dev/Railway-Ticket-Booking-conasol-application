08a0dd382c56164c73afa0d102030117342d5087
Bikash sah <15850882+bikashsah1@users.noreply.github.com>
2026-05-19 03:29:59 +0530

    Merge remote-tracking branch 'origin/main'

--- src/entites/Train.java
+++ src/entites/Train.java
@@ -1,8 +1,30 @@
 package entites;
 
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+
 public class Train {
     private String trainName;
     private String trainNumber;
+    private List<List<Integer>> seats;
+    private Map<String, Time> stationTime;
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public Map<String, Time> getStationTime() {
+        return stationTime;
+    }
+
+    public void setStationTime(Map<String, Time> stationTime) {
+        this.stationTime = stationTime;
+    }
 
     public Train(String trainName, String trainNumber) {
         this.trainName = trainName;
