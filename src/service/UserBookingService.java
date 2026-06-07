3204cd8a56e2863232bb27f32fdaddf535920f53
Bikash sah <bikashcod@gmail.com>
2026-06-07 23:24:25 +0530

    Add service class

--- src/service/UserBookingService.java
+++ src/service/UserBookingService.java
@@ -1,4 +1,11 @@
 package service;
 
+import entites.User;
+
 public class UserBookingService {
+    private User user;
+
+    public UserBookingService(User user) {
+        this.user = user;
+    }
 }
